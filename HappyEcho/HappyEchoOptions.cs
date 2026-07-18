601f8a2c264089e8bc6ee9405aded855008c7ea8
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-18 19:04:09 -0400

    set

--- HappyEcho/HappyEchoOptions.cs
+++ HappyEcho/HappyEchoOptions.cs
@@ -9,11 +9,11 @@ namespace HappyEcho;
 public sealed class HappyEchoOptions
 {
     public const string SectionName = "Echo";
-    public string ListenAddress { get; init; } = "127.0.0.1";
-    public int Port { get; init; } = 7;
-    public int MaxConcurrentConnections { get; init; } = 64;
-    public int RequestTimeoutSeconds { get; init; } = 15;
-    public long MaxBytesPerConnection { get; init; } = 1_048_576;
-    public string? TelemetryIgnoredRemoteAddress { get; init; }
-    public bool BlockLoopbackConnections { get; init; } = false;
+    public string ListenAddress { get; set; } = "127.0.0.1";
+    public int Port { get; set; } = 7;
+    public int MaxConcurrentConnections { get; set; } = 64;
+    public int RequestTimeoutSeconds { get; set; } = 15;
+    public long MaxBytesPerConnection { get; set; } = 1_048_576;
+    public string? TelemetryIgnoredRemoteAddress { get; set; }
+    public bool BlockLoopbackConnections { get; set; } = false;
 }
