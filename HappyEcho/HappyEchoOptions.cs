cb4b4371ca068ecb7cf1c3d288a2c1d9c630a086
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-22 21:38:44 -0400

    Adopt shared TCP server options contract

--- HappyEcho/HappyEchoOptions.cs
+++ HappyEcho/HappyEchoOptions.cs
@@ -1,19 +1,25 @@
 ﻿/*
- * Happy Echo Server
+ * Happy Echo Service
  * Copyright (c) 2026 Kyle Givler
  * Licensed under the MIT License.
  */
 
+using JoyfulReaperLib.TcpServer;
+
 namespace HappyEcho;
 
-public sealed class HappyEchoOptions
+public sealed class HappyEchoOptions : ITcpServerOptions
 {
     public const string SectionName = "Echo";
+
     public string ListenAddress { get; set; } = "127.0.0.1";
     public int Port { get; set; } = 7;
     public int MaxConcurrentConnections { get; set; } = 64;
     public int RequestTimeoutSeconds { get; set; } = 15;
     public long MaxBytesPerConnection { get; set; } = 1_048_576;
     public string? TelemetryIgnoredRemoteAddress { get; set; }
-    public bool BlockLoopbackConnections { get; set; } = false;
+    public bool BlockLoopbackConnections { get; set; }
+
+    ConnectionLimitBehavior ITcpServerOptions.ConnectionLimitBehavior =>
+        JoyfulReaperLib.TcpServer.ConnectionLimitBehavior.Reject;
 }
